83bf2749733a8c2ac24e2eae0decc7959f9c67e7
bussem <martin.busse@gmail.com>
2015-04-21 13:10:55 -0400

    Cachematrix script v1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of functions to set the value of a matrix,
+## get, set the value of its inverse and calculate the inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inver) inv <<- inver
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks if the inverse of the matrix is avail,
+## if not, it will calcualte it and set all the right values. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached inverse")
+    return(inv)
+  }
+  matrix <- x$get()
+  inv <- solve(matrix,...)
+  x$setinv(inv)
+  inv
 }
+
+
+## to check code:
+## testm <- matrix(c(1,2,3,4), nrow=2, ncol=2, byrow = TRUE)
+## testmC <- makeCacheMatrix(testm)
+## testmI <- cacheSolve(testmC)
+## testm %*% testmI
+## this should give the identity matrix
